fbe852f19f79c0915e2007175512e7001df8d56e
Breno Tomé <breno.tome@hotmail.com>
2019-08-09 21:23:37 -0300

    +layout +ts

--- Ionic/src/app/app-routing.module.ts
+++ Ionic/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
-  { path: 'post', loadChildren: './post/post.module#PostPageModule' }
+  { path: 'post/:id', loadChildren: './post/post.module#PostPageModule' }
 ];
 
 @NgModule({
--- Ionic/src/app/post/post.page.ts
+++ Ionic/src/app/post/post.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPage implements OnInit {
 
-  constructor() { }
+  constructor(public postService: PostService, private route: ActivatedRoute) { }
+  post={
+    title:"Um Titulo bem bonito",
+    image:"../../assets/img.jpeg",
+    date:"28 de Agosto de 1999",
+    text:"Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Quem num gosta di mim que vai caçá sua turmis! Detraxit consequat et quo num tendi nada. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis"
+  }
+  
+  getPost(id): void{
+    this.postService.getPost(id).subscribe(
+      (res) => {
+        console.log(res);
+        this.post = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit() {
+
+    this.route.params.subscribe(
+      params =>{
+        console.log(params['id']);
+        // this.getPost(params['id']);
+      }
+    );
   }
 
 }
