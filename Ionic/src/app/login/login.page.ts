f7470dc3b0462b109cf119e7e6eddeea4085bec6
PolyTadeu <polytadeu@gmail.com>
2019-08-10 13:24:08 -0300

    #medoto de login no ts

--- Ionic/src/app/login/login.page.ts
+++ Ionic/src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{FormGroup ,FormBuilder,Validators} from '@angular/forms';
 import {Router} from '@angular/router';
+// import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,28 @@ import {Router} from '@angular/router';
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
-
-  constructor(public formbuilder:FormBuilder, private router: Router) { 
+  //declarar  public usersService: UsersService no construtor
+  constructor(public formbuilder:FormBuilder, private router: Router  ) { 
     this.loginForm = this.formbuilder.group({
   		username: [null, [Validators.required]],
   		password: [null, [Validators.required, Validators.minLength(8)]],
   	});
   }
+  // loginUsuario( form ) {
 
+  // 	if ( form.status == "VALID" ) {
+
+  // 		this.userService.logarUsuario( form.value ).subscribe(
+  // 			(res) => {
+	// 			console.log( res.message );
+	// 			localStorage.setItem( 'userToken', res.data.token );
+	// 			this.router.navigate(['reservas']);
+  // 			}
+  // 		);
+
+  // 	}
+
+  // }
   ngOnInit() {
   }
 
